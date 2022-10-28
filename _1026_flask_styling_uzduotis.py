c85e3f08ba9e5b806f239cd7a9b2670c3c603cf2
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-10-28 12:20:46 +0300

    task flask container, grid, margin, padding etc.

--- _1026_flask_styling_uzduotis.py
+++ _1026_flask_styling_uzduotis.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/zalia_balta")
 def home():
     return render_template("styling/zalia_balta.html")
+
+@app.route("/vikipedija")
+def vikipedija():
+    return render_template("styling/vikipedija.html")
 
 if __name__=="__main__":
     app.run(debug=True)
